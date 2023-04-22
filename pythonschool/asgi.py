5fc3f6b86809dc905c9633a31ef14982cce87b34
James Njoroge <reecejames934@gmail.com>
2023-04-22 21:55:39 +0300

    Initial commit

---
+++ pythonschool/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pythonschool project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonschool.settings')
+
+application = get_asgi_application()
